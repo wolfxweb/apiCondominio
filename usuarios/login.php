d4c6aa3062b9ede77b846840461f1a5add62dfae
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2022-12-23 11:30:30 -0300

    retorno login

--- usuarios/login.php
+++ usuarios/login.php
@@ -1,20 +1,15 @@
 <?php
 include_once '../config/header.php';
-include_once '../config/database.php';
 include_once '../objects/usuarios.php';
 include_once '../notification/response.php';
-include_once '../token/token.php';
-include_once '../jwt/BeforeValidException.php';
-include_once '../jwt/ExpiredException.php';
-include_once '../jwt/SignatureInvalidException.php';
-include_once '../jwt/JWT.php';
-use \Firebase\JWT\JWT;
-$database = new Database(); 
-$db = $database->getConnection();
+
  
 
 $data = json_decode(file_get_contents("php://input"));
 if(!empty($data->usu_email) && !empty($data->usu_password)){
     $user = new Usuarios($data);
     $user->validaLogin();
+ }else{
+    Response::response(CAMPOS_INVALIDOS,"",REQ_ERROR_DADOS_ENVIADO,ERROR,0);
+    return false;
  }
